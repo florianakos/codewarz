5398c25474b3a7b7d5fc06fe74042f8bcf0c524b
Florian Akos Szabo <florian.akos.szabo@gmail.com>
2019-11-10 21:12:15 +0100

    concurrent version added

---
+++ drop_the_bass/main-concurrent.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+  "bufio"
+  "fmt"
+  "os"
+  "io/ioutil"
+  "runtime"
+  "strings"
+  "sync"
+  "os/exec"
+  "encoding/base64"
+)
+
+// error handling function
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// helper func to look up a given string in dictionary (location depends on OS)
+func dictLookup(word string) bool {
+	var dictLocation string
+	if runtime.GOOS == "darwin" {
+    dictLocation = "american-english"
+	} else {
+		dictLocation = "/usr/share/dict/american-english"
+	}
+	_, err := exec.Command("grep", "-w", word, dictLocation).Output()
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+// helper func to decode a base64 encoded string (append necessary = at end to make it valid...)
+func decode(src string) string {
+	data, err := base64.StdEncoding.DecodeString(src)
+	if err != nil {
+		src2 := src + "="
+		data2, err2 := base64.StdEncoding.DecodeString(src2)
+		if err2 != nil {
+			src3 := src2 + "="
+			data3, _ := base64.StdEncoding.DecodeString(src3)
+			return string(data3)
+		}
+		return string(data2)
+	}
+	return string(data)
+}
+
+func decryptWords(strInput string) {
+  file := strings.NewReader(strInput)
+
+  // do I need buffered channels here?
+  jobs := make(chan string)
+  results := make(chan string)
+
+  // I think we need a wait group, not sure.
+  wg := new(sync.WaitGroup)
+
+  // start up some workers that will block and wait?
+  for w := 1; w <= 5; w++ {
+    wg.Add(1)
+    go matchTelephoneNumbers(jobs, results, wg)
+  }
+
+  // Go over a file line by line and queue up a ton of work
+  go func() {
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+      // Later I want to create a buffer of lines, not just line-by-line here ...
+      jobs <- scanner.Text()
+    }
+    close(jobs)
+  }()
+
+  // Now collect all the results...
+  // But first, make sure we close the result channel when everything was processed
+  go func() {
+    wg.Wait()
+    close(results)
+  }()
+
+  // Add up the results from the results channel.
+  for v := range results {
+    fmt.Println(v)
+  }
+}
+
+func matchTelephoneNumbers(jobs <-chan string, results chan<- string, wg *sync.WaitGroup) {
+  // Decreasing internal counter for wait-group as soon as goroutine finishes
+  defer wg.Done()
+
+  // eventually I want to have a []string channel to work on a chunk of lines not just one line of text
+  for j := range jobs {
+    line := decode(j)
+
+    // test and further decode until valid work is found
+    for !(dictLookup(line)) {
+      line = decode(line)
+    }
+    results <- line
+  }
+}
+
+func main() {
+  // check if filename in arg is present
+	if (len(os.Args) - 1) != 1 {
+		fmt.Println("ERROR: missing argument!")
+		os.Exit(1)
+	}
+
+	// open file for reading
+	f, err := os.Open(os.Args[1:][0])
+	check(err)
+
+  input, err := ioutil.ReadAll(f)
+
+  file := strings.NewReader(string(input))
+
+  // do I need buffered channels here?
+  jobs := make(chan string)
+  results := make(chan string)
+
+  // I think we need a wait group, not sure.
+  wg := new(sync.WaitGroup)
+
+  // start up some workers that will block and wait?
+  for w := 1; w <= 5; w++ {
+    wg.Add(1)
+    go matchTelephoneNumbers(jobs, results, wg)
+  }
+
+  // Go over a file line by line and queue up a ton of work
+  go func() {
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+      // Later I want to create a buffer of lines, not just line-by-line here ...
+      jobs <- scanner.Text()
+    }
+    close(jobs)
+  }()
+
+  // Now collect all the results...
+  // But first, make sure we close the result channel when everything was processed
+  go func() {
+    wg.Wait()
+    close(results)
+  }()
+
+  // Add up the results from the results channel.
+  for v := range results {
+    fmt.Println(v)
+  }
+}
--- drop_the_bass/main.go
+++ drop_the_bass/main.go
@@ -6,13 +6,16 @@ import "os/exec"
 import "bufio"
 import "encoding/base64"
 import "runtime"
+import "sync"
 
+// error handling function
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
+// helper func to look up a given string in dictionary (location depends on OS)
 func dictLookup(word string) bool {
 	var dictLocation string
 	if runtime.GOOS == "darwin" {
@@ -27,6 +30,7 @@ func dictLookup(word string) bool {
 	return true
 }
 
+// helper func to decode a base64 encoded string (append necessary = at end to make it valid...)
 func decode(src string) string {
 	data, err := base64.StdEncoding.DecodeString(src)
 	if err != nil {
@@ -43,20 +47,39 @@ func decode(src string) string {
 }
 
 func main() {
-	line := ""
-	if (len(os.Args) - 1) == 1 {
-		file, err := os.Open(os.Args[1:][0])
-		check(err)
-		scanner := bufio.NewScanner(file)
+	// global variable to enable concurrency
+	var wg sync.WaitGroup
 
-		for scanner.Scan() {
+	// check if filename in arg is present
+	if (len(os.Args) - 1) != 1 {
+		fmt.Println("ERROR: missing argument!")
+		os.Exit(1)
+	}
+
+	// open file for reading
+	file, err := os.Open(os.Args[1:][0])
+	check(err)
+
+	// read it for scanning line by line
+	scanner := bufio.NewScanner(file)
+	var line string
+
+	// loop over lines until they exist
+	for scanner.Scan() {
+
+		wg.Add(1)
+		go func() {
+			fmt.Println("Started new goroutine...")
+			// decode the current line
 			line = decode(scanner.Text())
+
+			// test and further decode until valid work is found
 			for !(dictLookup(line)) {
 				line = decode(line)
 			}
 			fmt.Println(line)
-		}
-	} else {
-		fmt.Println("ERROR: missing argument!")
+      wg.Done()
+		}()
+		wg.Wait()
 	}
 }
