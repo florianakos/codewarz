1a812c6d56d5dfa00a11898306ff56f61ed07aed
Florian Akos Szabo <florian.akos.szabo@gmail.com>
2019-11-11 23:43:59 +0100

    gofmt

--- simple_cipher/simplecipher.go
+++ simple_cipher/simplecipher.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "io/ioutil"
-  "strings"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 func main() {
-  // check if filename in arg is present
+	// check if filename in arg is present
 	if (len(os.Args) - 1) != 1 {
 		fmt.Println("ERROR: missing argument!")
 		os.Exit(1)
 	}
 
-  // read whole file as text into byte array
-  byteString, err := ioutil.ReadFile(os.Args[1])
-  if err != nil {
-      fmt.Print(err)
-  }
+	// read whole file as text into byte array
+	byteString, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Print(err)
+	}
 
-  // convert byte array to string
-  strText := string(byteString)
+	// convert byte array to string
+	strText := string(byteString)
 
-  // extract sentences by splitting on dot
-  sentences := strings.Split(strText, ".")
+	// extract sentences by splitting on dot
+	sentences := strings.Split(strText, ".")
 
-  // extract first word of each sentence, plus clean from whitespace.
-  for _, v := range(sentences) {
-    word := strings.Split(strings.TrimSpace(v), " ")[0]
-    if word != "" {
-      fmt.Printf("%s ", word)
-    }
-  }
+	// extract first word of each sentence, plus clean from whitespace.
+	for _, v := range sentences {
+		word := strings.Split(strings.TrimSpace(v), " ")[0]
+		if word != "" {
+			fmt.Printf("%s ", word)
+		}
+	}
 }
