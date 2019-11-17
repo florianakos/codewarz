6263fa3581730d47f313515c46f4994a36b1b546
Florian Akos Szabo <florian.akos.szabo@gmail.com>
2019-11-17 21:18:27 +0100

    gofmt

--- mascii-u-sumthin/mascii.go
+++ mascii-u-sumthin/mascii.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"bufio"
+	"encoding/hex"
 	"fmt"
 	"os"
 	"strconv"
 	"strings"
-  "encoding/hex"
 )
 
 // error handling function
@@ -17,16 +17,16 @@ func check(e error) {
 }
 
 func convertHex(hx string) string {
-    bs, err := hex.DecodeString(strings.Split(hx, "x")[1])
-    if err != nil {
-        fmt.Println(err)
-        return ""
-    }
-    return string(bs)
+	bs, err := hex.DecodeString(strings.Split(hx, "x")[1])
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return string(bs)
 }
 
 func convertBinary(bin string) string {
-  input := strings.Split(bin, "b")[1]
+	input := strings.Split(bin, "b")[1]
 	b := make([]byte, 0)
 	for _, s := range strings.Fields(input) {
 		n, _ := strconv.ParseUint(s, 2, 8)
@@ -36,12 +36,12 @@ func convertBinary(bin string) string {
 }
 
 func convertOctal(oct string) string {
-  output, err := strconv.ParseInt(oct, 8, 64)
-  if err != nil {
-    fmt.Println(err)
-    return ""
-  }
-  return string(output)
+	output, err := strconv.ParseInt(oct, 8, 64)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return string(output)
 }
 
 func main() {
@@ -55,28 +55,28 @@ func main() {
 	file, err := os.Open(os.Args[1:][0])
 	check(err)
 
-  // run scanner on input
+	// run scanner on input
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-    tokens := strings.Split(strings.TrimSpace(scanner.Text()), " ")
-    // parse each token as either hex, binary, octal or decial
-    for _, val := range tokens {
-      if len(val) != 0 {
-        // token is hexadecimal string
-        if strings.Contains(val, "x") {
-          fmt.Print(convertHex(val))
-        // token is binary string
-        } else if strings.Contains(val, "b") {
-          fmt.Print(convertBinary(val))
-        // token is octal string
-        } else if strings.Index(val, "0") == 0 {
-          fmt.Print(convertOctal(val))
-        // token is decimal string
-        } else {
-          tmp, _ :=  strconv.Atoi(val)
-          fmt.Print(string(tmp))
-        }
-      }
-    }
+		tokens := strings.Split(strings.TrimSpace(scanner.Text()), " ")
+		// parse each token as either hex, binary, octal or decial
+		for _, val := range tokens {
+			if len(val) != 0 {
+				// token is hexadecimal string
+				if strings.Contains(val, "x") {
+					fmt.Print(convertHex(val))
+					// token is binary string
+				} else if strings.Contains(val, "b") {
+					fmt.Print(convertBinary(val))
+					// token is octal string
+				} else if strings.Index(val, "0") == 0 {
+					fmt.Print(convertOctal(val))
+					// token is decimal string
+				} else {
+					tmp, _ := strconv.Atoi(val)
+					fmt.Print(string(tmp))
+				}
+			}
+		}
 	}
 }
